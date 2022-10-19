cd1fbcfb2a0c4f4c329f0a4bcb40f61e4f8aa608
utnesz <90829509+utnesz@users.noreply.github.com>
2022-10-19 22:19:27 +0200

    Ready

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,29 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+  {
+    id: 16,
+    name: "Susulyka",
+    sex: "male",
+    age: 225,
+    health: 98,
+  },
+  {
+    id: 46,
+    name: "Rumcajsz",
+    sex: "male",
+    age: 225,
+    health: 98,
+  },
+  {
+    id: 26,
+    name: "Amazon",
+    sex: "female",
+    age: 225,
+    health: 4000,
+  }
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +40,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+  { id: 45, name: "Mari", wings: 2, wheels: 4, clan: "alca" },
+  { id: 45, name: "Gizi", wings: 4, wheels: 4, clan: "transformer" },
+  { id: 45, name: "Geza", wings: 6, wheels: 4, clan: "alca" },
+];
--- src/transformer.ts
+++ src/transformer.ts
@@ -1,4 +1,4 @@
-import { ITransformer } from './hero-interface';
+import { ITransformer } from "./hero-interface";
 
 /**
  * Állítsd be helyesen a TransformerHero osztályt!
@@ -7,6 +7,27 @@ import { ITransformer } from './hero-interface';
  * Deklaráld benne az interfész által megkövetelt változókat.
  * Definiáld a konstruktort és kérj be minden változót, majd állítsd be őket.
  */
-export class TransformerHero {
-    
+export class TransformerHero implements ITransformer {
+  id: number;
+  name: string;
+  wings: number;
+  wheels: number;
+  clan: string;
+
+  constructor(
+    ide: number,
+    nev: string,
+    szarnyak: number,
+    kerekek: number,
+    klan: string,
+    foto?: string
+  ) {
+    this.id = ide;
+    this.name = nev;
+    this.wings = szarnyak;
+    this.wheels = kerekek;
+    this.clan = klan;
+  }
 }
+
+const newTransformer = new TransformerHero(16, "Mikkamakka",  2, 4, "alca");
